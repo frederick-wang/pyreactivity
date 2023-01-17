e83b2c39d316c78ab8daa17f60f91f93388fc067
Zhaoji Wang <hwoam@outlook.com>
2023-01-17 15:19:51 +0800

    fix(patches): fix the support of Ref for json
    1. Add support of json.dump();
    2. Add support of `default` param for json.dump() and json.dumps().

--- src/reactivity/patches/__init__.py
+++ src/reactivity/patches/__init__.py
@@ -27,6 +27,14 @@ def __patch_json_dumps():
     raw_json_dumps = json.dumps
 
     def dumps(*args: Any, **kwargs: Any):
+        if 'default' in kwargs:
+            raw_default = kwargs['default']
+
+            def default(o: object):
+                return cast(Ref[Any], o).value if is_ref(o) else raw_default(o)
+
+            kwargs['default'] = default
+
         raw_class = kwargs['cls'] if 'cls' in kwargs else json.JSONEncoder
         if raw_class not in __json_encoder_map:
             class_name = raw_class.__name__
@@ -36,13 +44,42 @@ def __patch_json_dumps():
                 {'metaclass': PyReactivityJSONEncoderMetaClass},
             )
         kwargs['cls'] = __json_encoder_map[raw_class]
+
         return raw_json_dumps(*args, **kwargs)
 
     json.dumps = dumps
 
 
+def __patch_json_dump():
+    raw_json_dump = json.dump
+
+    def dump(*args: Any, **kwargs: Any):
+        if 'default' in kwargs:
+            raw_default = kwargs['default']
+
+            def default(o: object):
+                return cast(Ref[Any], o).value if is_ref(o) else raw_default(o)
+
+            kwargs['default'] = default
+
+        raw_class = kwargs['cls'] if 'cls' in kwargs else json.JSONEncoder
+        if raw_class not in __json_encoder_map:
+            class_name = raw_class.__name__
+            __json_encoder_map[raw_class] = types.new_class(
+                class_name,
+                (raw_class,),
+                {'metaclass': PyReactivityJSONEncoderMetaClass},
+            )
+        kwargs['cls'] = __json_encoder_map[raw_class]
+
+        return raw_json_dump(*args, **kwargs)
+
+    json.dump = dump
+
+
 def patch():
     __patch_json_dumps()
+    __patch_json_dump()
 
 
 __all__ = ['patch']
--- tests/test_json.py
+++ tests/test_json.py
@@ -1,3 +1,4 @@
+import io
 import json
 
 from reactivity import reactive, ref
@@ -7,18 +8,27 @@ from reactivity import reactive, ref
 def test_reactive_json_dumps_reactive():
     obj = reactive({'foo': {'bar': [1, 2, 3]}, 'baz': 1, 'a': 'Hello', 'b': True})
     assert json.dumps(obj) == '{"foo": {"bar": [1, 2, 3]}, "baz": 1, "a": "Hello", "b": true}'
+    f = io.StringIO()
+    json.dump(obj, f)
+    assert f.getvalue() == '{"foo": {"bar": [1, 2, 3]}, "baz": 1, "a": "Hello", "b": true}'
 
 
 # should be able to json.dumps (ref.value)
 def test_reactive_json_dumps_ref_value():
     obj = ref({'foo': {'bar': [1, 2, 3]}, 'baz': 1, 'a': 'Hello', 'b': True})
     assert json.dumps(obj.value) == '{"foo": {"bar": [1, 2, 3]}, "baz": 1, "a": "Hello", "b": true}'
+    f = io.StringIO()
+    json.dump(obj.value, f)
+    assert f.getvalue() == '{"foo": {"bar": [1, 2, 3]}, "baz": 1, "a": "Hello", "b": true}'
 
 
 # should be able to json.dumps (ref)
 def test_reactive_json_dumps_ref():
     obj = ref({'foo': ref({'bar': [1, 2, 3]}), 'baz': ref(1), 'a': 'Hello', 'b': True})
     assert json.dumps(obj) == '{"foo": {"bar": [1, 2, 3]}, "baz": 1, "a": "Hello", "b": true}'
+    f = io.StringIO()
+    json.dump(obj, f)
+    assert f.getvalue() == '{"foo": {"bar": [1, 2, 3]}, "baz": 1, "a": "Hello", "b": true}'
 
 
 # should be able to json.dumps (ref) when using custom encoder
@@ -33,6 +43,24 @@ def test_reactive_json_dumps_ref_custom_encoder():
 
     obj = ref({'foo': ref({'bar': [ref(1), 2, 3]}), 'baz': ref(1 + 1j), 'c': 2 + 1j})
     assert json.dumps(obj, cls=ComplexEncoder) == '{"foo": {"bar": [1, 2, 3]}, "baz": [1.0, 1.0], "c": [2.0, 1.0]}'
+    f = io.StringIO()
+    json.dump(obj, f, cls=ComplexEncoder)
+    assert f.getvalue() == '{"foo": {"bar": [1, 2, 3]}, "baz": [1.0, 1.0], "c": [2.0, 1.0]}'
+
+
+# should be able to jsom.dumps (ref) when set default parameter
+def test_reactive_json_dumps_ref_default_parameter():
+
+    def complex_handler(obj):
+        if isinstance(obj, complex):
+            return [obj.real, obj.imag]
+        raise TypeError(f'Object of type {obj.__class__.__name__} is not JSON serializable')
+
+    obj = ref({'foo': ref({'bar': [ref(1), 2, 3]}), 'baz': ref(1 + 1j), 'c': 2 + 1j})
+    assert json.dumps(obj, default=complex_handler) == '{"foo": {"bar": [1, 2, 3]}, "baz": [1.0, 1.0], "c": [2.0, 1.0]}'
+    f = io.StringIO()
+    json.dump(obj, f, default=complex_handler)
+    assert f.getvalue() == '{"foo": {"bar": [1, 2, 3]}, "baz": [1.0, 1.0], "c": [2.0, 1.0]}'
 
 
 # should be able to json.dumps int wrapped in ref
@@ -45,15 +73,24 @@ def test_reactive_json_dumps_ref_int():
 def test_reactive_json_dumps_ref_str():
     obj = ref('Hello')
     assert json.dumps(obj) == '"Hello"'
+    f = io.StringIO()
+    json.dump(obj, f)
+    assert f.getvalue() == '"Hello"'
 
 
 # should be able to json.dumps bool wrapped in ref
 def test_reactive_json_dumps_ref_bool():
     obj = ref(True)
     assert json.dumps(obj) == 'true'
+    f = io.StringIO()
+    json.dump(obj, f)
+    assert f.getvalue() == 'true'
 
 
 # should be able to json.dumps None wrapped in ref
 def test_reactive_json_dumps_ref_none():
     obj = ref(None)
     assert json.dumps(obj) == 'null'
+    f = io.StringIO()
+    json.dump(obj, f)
+    assert f.getvalue() == 'null'
